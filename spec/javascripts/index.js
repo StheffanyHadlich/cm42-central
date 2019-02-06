a342a455c89eb9fe9adab11a61b5b807db9aa49b
Adriano Costa <adrianokosta001@gmail.com>
2019-02-06 11:37:15 -0200

    Update React to v6

--- spec/javascripts/index.js
+++ spec/javascripts/index.js
@@ -3,9 +3,13 @@ require('./polyfills/objectAssign');
 require('./extra/jasmine-sinon');
 require('./extra/jasmine-jquery');
 require('es6-promise/auto');
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 var context = require.context('.', true, /.+_spec\.js$/);
 
 context.keys().forEach(context);
+
+Enzyme.configure({ adapter: new Adapter() });
 
 module.exports = context;
